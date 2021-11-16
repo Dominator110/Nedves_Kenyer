69b94dce41880fbc8409f71929abd5f58039f108
Gábor <gabo.imre@gmail.com>
2021-11-16 14:24:03 +0100

    ez kapcsolja össze a progikat

---
+++ kapcs.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$con = mysqli_connect("localhost", "root", "", "idojaras");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Sikertelen kapcsolódás: " . mysqli_connect_error();
+}
+
+?>
